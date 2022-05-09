a344976c579392925e881a5a16224663947b6a85
aun mirza <aunabbasmirza@gmail.com>
2022-05-09 18:01:24 -0400

    Updates

---
+++ precache-manifest.9ae4fcef86fb8db4afdd251cfca7d16d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "34c7678a325862963330",
+    "url": "/static/css/main.116932e7.chunk.css"
+  },
+  {
+    "revision": "34c7678a325862963330",
+    "url": "/static/js/main.34c7678a.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "b23a0157d65b84d42061",
+    "url": "/static/js/2.b23a0157.chunk.js"
+  },
+  {
+    "revision": "b29c9e157adc6ecf5862",
+    "url": "/static/js/runtime~main.b29c9e15.js"
+  },
+  {
+    "revision": "29d265c97f9981cf5bed022a7e5b7db1",
+    "url": "/index.html"
+  }
+];
